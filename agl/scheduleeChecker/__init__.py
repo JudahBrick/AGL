c4d64a1c3ccd4b12a90a08ece61f80d818945133
Yehuda Brick <yehudabrick@yehudas-mbp-7.lan>
2021-03-19 18:39:02 -0400

    pre shabbos push

--- agl/scheduleeChecker/__init__.py
+++ agl/scheduleeChecker/__init__.py
@@ -2,6 +2,8 @@ import pandas
 
 games_played = pandas.read_csv("games played.csv")
 print(games_played.columns)
-players = games_played['player']
+players = games_played['Player']
 
 # players.it
+num = games_played.query('Player == "Brick"')
+print(num)
